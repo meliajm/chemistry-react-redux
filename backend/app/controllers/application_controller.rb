25fb4010a68c1b224961482e82ee86443dd9fdb4
github username <meliajm@gmail.com>
2020-05-04 14:39:59 -0700

    can connect to server and see json data!

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,10 +1,22 @@
-class ApplicationController < ActionController::API include ActionController::Cookies
-
+class ApplicationController < ActionController::API
+    include ActionController::Cookies
+  
     def current_user
-        User.find(session[:user_id])
+      User.find_by(id: session[:user_id])
     end
-
+  
     def logged_in?
-        !!session[:user_id]
+      !!current_user
     end
 end
+
+# class ApplicationController < ActionController::API include ActionController::Cookies
+
+#     def current_user
+#         User.find(session[:user_id])
+#     end
+
+#     def logged_in?
+#         !!session[:user_id]
+#     end
+# end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  post '/login', to: 'sessions#create'
-  post '/logout', to: 'sessions#logout'
-  get '/logout', to: 'sessions#destroy'
-  get "/get_current_user", to: "sessions#get_current_user"
-  post '/signin', to: 'sessions#new'
-
   namespace :api do
     namespace :v1 do
       resources :users
-      resources :atoms      
+      resources :atoms 
+      post '/login', to: 'sessions#create'
+      post '/logout', to: 'sessions#logout'
+      # get '/logout', to: 'sessions#destroy'
+      get "/get_current_user", to: "sessions#get_current_user"
+      post '/signin', to: 'sessions#new'     
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
