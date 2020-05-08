23b8af35f2505bc671280e080fbc7603a637a1df
github username <meliajm@gmail.com>
2020-05-08 10:54:44 -0700

    update users controller

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -16,7 +16,6 @@ class Api::V1::UsersController < ApplicationController
   # POST /users
   def create
     @user = User.new(user_params)
-
     if @user.save
       session[:user_id] = @user.id
       render json: {
