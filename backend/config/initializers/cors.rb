aed1c5bc3a2bded7cca7573f3b85b567b06cbe57
github username <meliajm@gmail.com>
2020-05-17 18:48:01 -0700

    new frontend to cors

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', 'up-and-atom.netlify.app'
+    origins 'up-and-atom.netlify.app', 'localhost:3000', 'upandatomlab.netlify.app'
 
     resource '*',
       headers: :any,
